cd6357f06288ef83b1c106e224e0b5d1ab6e41aa
Tom M <tom.mcmanus@gmail.com>
2022-12-01 15:25:26 -0500

    Initial commit.

---
+++ day1/day1.js
@@ -0,0 +1,37 @@
+
+const fs = require('node:fs');
+
+const readline = require('node:readline');
+
+const rl = readline.createInterface(fs.createReadStream('./input1'));
+
+let current_elf_calories = [];
+let all_calories = [-1];
+
+rl.on('line', (line) => {
+		if("" == line) {
+				let total_calories = 0;
+				current_elf_calories.forEach((calorie) => {
+						total_calories += calorie;
+				});
+
+				all_calories.every((this_one, idx) => {
+						if(total_calories >= this_one) {
+								all_calories.splice(idx, 0, total_calories);
+								return false;
+						}
+						return true;
+				});
+				current_elf_calories = [];
+		}
+		else {
+				current_elf_calories.push(parseInt(line));
+		}
+});
+
+rl.on('close', ()=> {
+		console.log("EOF");
+		console.log(`Highest calorie count is ${all_calories[0]}`);
+		console.log(`3 highest calorie counts are ${all_calories[0]} :: ${all_calories[1]} :: ${all_calories[2]} :: ${all_calories.slice(0, 3).reduce((sum, value) => sum + value)}`);
+});
+
